b48d9abfefcd2395e85c65e69c51fbf43a06652c
DESKTOP\TheBestWorker <thebestworkerinthecountry@gmail.com>
2021-03-17 12:25:08 +0300

    Added HUD class

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -4,10 +4,12 @@
 #include "STUGameModeBase.h"
 #include "Player/STUBaseCharacter.h"
 #include "Player/STUPlayerController.h"
+#include "UI/STUGameHUD.h"
 
 ASTUGameModeBase::ASTUGameModeBase()
 {
     DefaultPawnClass = ASTUBaseCharacter::StaticClass();
     PlayerControllerClass = ASTUPlayerController::StaticClass();
+    HUDClass = ASTUGameHUD::StaticClass();
 }
 
