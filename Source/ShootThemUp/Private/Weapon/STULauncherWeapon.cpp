1dc6c6dce56d9e53147206521c9738d2025e7b91
DESKTOP\TheBestWorker <thebestworkerinthecountry@gmail.com>
2021-03-28 23:46:32 +0300

    Added class Projectile for Launcher Weapon

--- Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
@@ -1,5 +1,22 @@
 // Shoot Them Up Game. All Rights Reserved
 
-
 #include "Weapon/STULauncherWeapon.h"
+#include "Weapon/STUProjectile.h"
+#include "Kismet/GameplayStatics.h"
+
+void ASTULauncherWeapon::StartFire()
+{
+    MakeShot();
+}
+
+void ASTULauncherWeapon::MakeShot()
+{
+    const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleWorldLocation());
+    auto Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(GetWorld(), ProjectileClass, SpawnTransform);
+    //set projectile params
+
+    UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
+}
+
+
 
