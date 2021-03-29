7810f37d72fdd4c788247e5108f341db34f51ae1
DESKTOP\TheBestWorker <thebestworkerinthecountry@gmail.com>
2021-03-29 23:19:13 +0300

    Added Projectile velocity

--- Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
+++ Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
@@ -2,6 +2,7 @@
 
 #include "Weapon/STUProjectile.h"
 #include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 
 ASTUProjectile::ASTUProjectile()
@@ -10,12 +11,18 @@ ASTUProjectile::ASTUProjectile()
     CollisionComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
     CollisionComponent->InitSphereRadius(5.0f);
     SetRootComponent(CollisionComponent);
+
+    MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComponent");
+    MovementComponent->InitialSpeed = 2000.0f;
+    MovementComponent->ProjectileGravityScale = 0.0f;
 }
 
 void ASTUProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	check(MovementComponent);
+    MovementComponent->Velocity = ShotDirection * MovementComponent->InitialSpeed;
+    SetLifeSpan(5.0f);
 }
 
 
